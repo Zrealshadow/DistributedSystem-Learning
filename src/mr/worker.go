e5386f513ac3c2b1b14a41009085f31d4b48d564
Zreal <zenglz_pro@163.com>
2021-06-29 17:25:07 +0800

    finish worker code

--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,10 +1,18 @@
 package mr
 
-import "fmt"
-import "log"
-import "net/rpc"
-import "hash/fnv"
-
+import (
+	"encoding/json"
+	"fmt"
+	"hash/fnv"
+	"io/ioutil"
+	"log"
+	"net/rpc"
+	"os"
+	"path/filepath"
+	"sort"
+	"strconv"
+	"time"
+)
 
 //
 // Map functions return a slice of KeyValue.
@@ -24,18 +32,147 @@ func ihash(key string) int {
 	return int(h.Sum32() & 0x7fffffff)
 }
 
-
 //
 // main/mrworker.go calls this function.
 //
 func Worker(mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
 
+	for {
+		task := CallAskTask()
+		switch task.taskType {
+		case MAPTASK:
+			mapOperator(mapf, task)
+
+		case REDUCETASK:
+			reduceOperator(reducef, task)
+
+		case WAIT:
+			time.Sleep(time.Duration(time.Second * 60))
+
+		case END:
+			fmt.Printf("All Tasks are finished")
+			return
+
+		default:
+			panic("Invalid task Type")
+		}
+
+	}
+
 	// Your worker implementation here.
 
 	// uncomment to send the Example RPC to the coordinator.
 	// CallExample()
 
+}
+
+//helper function for Worker
+func mapOperator(mapf func(string, string) []KeyValue, mapTask *TaskInfo) {
+	filename := mapTask.fileName
+	file, err := os.Open(filename)
+	if err != nil {
+		errorstr := fmt.Sprintf("the file %v dose not exist", filename)
+		panic(errorstr)
+	}
+	content, err := ioutil.ReadAll(file)
+
+	if err != nil {
+		errorstr := fmt.Sprintf("fail to read file %v", filename)
+		panic(errorstr)
+	}
+	file.Close()
+	nReduce := mapTask.nReduce
+	intermediate := mapf(filename, string(content))
+	outfiles := make([]*os.File, nReduce)
+	encoderList := make([]*json.Encoder, nReduce)
+	// initialize
+	for idx := 0; idx < nReduce; idx++ {
+		outfiles[idx], _ = ioutil.TempFile("mr-tmp", "mr-tmp-*")
+		encoderList[idx] = json.NewEncoder(outfiles[idx])
+	}
+
+	// write intermediate into outfiles
+	for _, kv := range intermediate {
+		index := ihash(kv.Key) % nReduce
+		enc := encoderList[index]
+		err := enc.Encode(&kv)
+		if err != nil {
+			fmt.Printf("File %v Key %v Value %v Error %v \n", mapTask.fileName, kv.Key, kv.Value, err)
+			panic("fail to write kv into file")
+		}
+	}
+
+	// rename the temport file into formal file.
+	outPrefix := "mr-tmp/mr-" + strconv.Itoa(mapTask.identifier) + "-"
+	for idx, file := range outfiles {
+		outname := outPrefix + strconv.Itoa(idx)
+		oldpath := filepath.Join(file.Name())
+		os.Rename(oldpath, outname)
+		file.Close()
+	}
+
+	//Ack
+	CallAckTask(mapTask.taskType)
+
+}
+
+func reduceOperator(reducef func(string, []string) string, reduceTask *TaskInfo) {
+	intermediate_prefix := "mr-tmp/mr-"
+	taskId := reduceTask.identifier
+	kvList := make([]KeyValue, 0)
+	for idx := 0; idx < reduceTask.nFiles; idx++ {
+		filepath := intermediate_prefix + strconv.Itoa(idx) + "-" + strconv.Itoa(taskId)
+		file, err := os.Open(filepath)
+		if err != nil {
+			fmt.Printf("File %v failed to open", filepath)
+			panic("Error in open file")
+		}
+		dec := json.NewDecoder(file)
+		for {
+			var kv KeyValue
+			if err := dec.Decode(&kv); err != nil {
+				break
+			}
+			kvList = append(kvList, kv)
+		}
+		file.Close()
+	}
+
+	// sort
+	sort.Slice(kvList, func(i, j int) bool {
+		return kvList[i].Key < kvList[j].Key
+	})
+
+	// compact
+	outTmpFile, err := ioutil.TempFile("mr-tmp", "mr-*")
+	if err != nil {
+		panic("failed to create temporary file")
+	}
+
+	k := ""
+	var v []string
+	for idx, kv := range kvList {
+		if k != kv.Key {
+			if idx != 0 {
+				ans := reducef(k, v)
+
+				// write into the file
+				fmt.Fprintf(outTmpFile, "%v %v\n", k, ans)
+			}
+
+			k = kv.Key
+			v = make([]string, 0)
+
+		}
+		v = append(v, kv.Value)
+	}
+
+	// rename the file
+	outfilename := "mr-out-" + strconv.Itoa(reduceTask.identifier)
+	os.Rename(filepath.Join(outTmpFile.Name()), outfilename)
+	outTmpFile.Close()
+	CallAckTask(reduceTask.taskType)
 }
 
 //
@@ -59,6 +196,27 @@ func CallExample() {
 
 	// reply.Y should be 100.
 	fmt.Printf("reply.Y %v\n", reply.Y)
+}
+
+func CallAskTask() *TaskInfo {
+	// declare an argument structure.
+	args := ExampleArgs{}
+	// declare a reply structure.
+	reply := TaskInfo{}
+
+	// send the RPC request, wait for the reply.
+	call("Coordinator.AskTask", &args, &reply)
+	return &reply
+}
+
+func CallAckTask(task_type int) *TaskInfo {
+	args := TaskInfo{
+		taskType: task_type,
+	}
+
+	reply := TaskInfo{}
+	call("Coordinator.AckTask", &args, &reply)
+	return &reply
 }
 
 //
